5b3b9545bc577e361e372be0261b0fadc6424727
hsiangting49 <157249687+hsiangting49@users.noreply.github.com>
2024-01-21 19:13:59 -0500

    Update scripts.js

--- _site/scripts.js
+++ _site/scripts.js
@@ -1,3 +1,7 @@
-document.getElementById('clickableImage').addEventListener('click', function() {
-    document.getElementById('displayText').innerText = 'Text changed!';
-});
+function showContent(celestial) {
+    const allContents = document.querySelectorAll('.celestial-content');
+    allContents.forEach(content => content.classList.remove('active'));
+
+    const selectedContent = document.getElementById(`${celestial}-content`);
+    selectedContent.classList.add('active');
+}
